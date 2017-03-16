3272a382381c72dfdd319f9b5690248e734ac1ea
lctsongas <ctsongas94@gmail.com>
2017-03-16 01:04:11 -0500

    Initial Files

--- ClientLauncher.py
+++ ClientLauncher.py
@@ -4,14 +4,10 @@ from Client import Client
 
 if __name__ == "__main__":
 	try:
-		# serverAddr = sys.argv[1]
-		# serverPort = sys.argv[2]
-		# rtpPort = sys.argv[3]
-		# fileName = sys.argv[4]
-		serverAddr = "192.168.0.10"
-		serverPort = 5060
-		rtpPort = 65200
-		fileName = "movie.Mjpeg"
+		serverAddr = sys.argv[1]
+		serverPort = sys.argv[2]
+		rtpPort = sys.argv[3]
+		fileName = sys.argv[4]	
 	except:
 		print "[Usage: ClientLauncher.py Server_name Server_port RTP_port Video_file]\n"	
 	
--- Server.py
+++ Server.py
@@ -6,14 +6,12 @@ class Server:
 	
 	def main(self):
 		try:
-			SERVER_PORT = 5060
-			# SERVER_PORT = int(sys.argv[1])
+			SERVER_PORT = int(sys.argv[1])
 		except:
 			print "[Usage: Server.py Server_port]\n"
 		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		rtspSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		rtspSocket.bind(('', SERVER_PORT))
-		rtspSocket.listen(10)        
+		rtspSocket.listen(5)        
 
 		# Receive client info (address,port) through RTSP/TCP session
 		while True:
